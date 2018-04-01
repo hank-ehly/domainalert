865e3775a70a822ca2f2a8bc863e2fea4121ebfc
Hank Ehly <henry.ehly@gmail.com>
2018-04-01 16:22:06 +0900

    Use mysql

--- domainalert/api/models.py
+++ domainalert/api/models.py
@@ -5,10 +5,25 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     email = models.EmailField()
 
+    class Meta:
+        ordering = ('created_at',)
+        db_table = 'api_users'
+
 
 class Domain(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         ordering = ('created_at',)
+        db_table = 'api_domains'
+
+
+class WatchedDomain(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    domain = models.ForeignKey('Domain', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
+        db_table = 'api_watched_domains'
--- domainalert/settings.py
+++ domainalert/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -27,6 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ENVIRONMENT = os.environ.get("ENV", 'development')
 
 # Application definition
 
@@ -71,17 +71,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'domainalert.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'domainalert',
+        'USER': 'domainalert',
+        'PASSWORD': 'domainalert',
+        'HOST': os.environ.get('DATABASES_default_PORT', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASES_default_PORT', '3306'),
+        'TEST': {
+            'NAME': 'domainalert_test',
+            'CHARSET': 'UTF8MB4'
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -101,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -114,7 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
