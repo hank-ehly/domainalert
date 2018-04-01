0b7e7274b46c1ac2b1bfa64699bcd908e4e2947d
Hank Ehly <henry.ehly@gmail.com>
2018-04-01 15:56:40 +0900

    Add some models

--- domainalert/api/models.py
+++ domainalert/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+
+
+class Domain(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('created_at',)
