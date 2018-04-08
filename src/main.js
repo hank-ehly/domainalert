6a25e8aef22dbce0d101f7feb615203e41a3f92f
Hank Ehly <henry.ehly@gmail.com>
2018-04-08 20:23:15 +0900

    Instantiate Vue app after FB sdk load

--- src/main.js
+++ src/main.js
@@ -6,33 +6,36 @@ import './resources'
 
 Vue.config.productionTip = false
 
-// eslint-disable-next-line no-new
-new Vue({
-    el: '#app',
-    router,
-    components: {App},
-    template: '<App/>'
-    // created() {
-    //     console.log('created main')
-    //
-    //     window.fbAsyncInit = function () {
-    //         FB.init({
-    //             appId: '775455055983360',
-    //             cookie: true,
-    //             xfbml: true,
-    //             version: 'v2.12'
-    //         })
-    //         FB.AppEvents.logPageView()
-    //     };
-    //     (function (d, s, id) {
-    //         var js, fjs = d.getElementsByTagName(s)[0]
-    //         if (d.getElementById(id)) {
-    //             return
-    //         }
-    //         js = d.createElement(s)
-    //         js.id = id
-    //         js.src = 'https://connect.facebook.net/en_US/sdk.js'
-    //         fjs.parentNode.insertBefore(js, fjs)
-    //     }(document, 'script', 'facebook-jssdk'))
-    // }
-})
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '775455055983360',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.12'
+    })
+    FB.AppEvents.logPageView()
+
+    // eslint-disable-next-line no-new
+    new Vue({
+        el: '#app',
+        router,
+        components: {App},
+        template: '<App/>',
+        created() {
+            if (process.env.NODE_ENV === 'development') {
+                console.log('[main] Created App')
+            }
+        }
+    })
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) {
+        return
+    }
+    js = d.createElement(s)
+    js.id = id
+    js.src = 'https://connect.facebook.net/en_US/sdk.js'
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
