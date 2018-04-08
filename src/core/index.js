2c1ff652a03e656949f8d22f482e7535e6515237
Hank Ehly <henry.ehly@gmail.com>
2018-04-08 20:25:48 +0900

    Rename file

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,4 @@
-export { default as AuthGuard } from './auth-guard'
+export { default as AuthGuard } from './auth.guard'
 export { default as facebookService } from './facebook.service'
 export { default as authService } from './auth.service'
 export { default as logger } from './logger'
